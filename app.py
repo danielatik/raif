2372118b9de67de277ab0c56fb6c03e007a042de
Daniel Atik <danielatik@users.noreply.github.com>
2023-08-04 22:17:08 -0400

    ddd

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("Ask anything about the Responible"):
+if prompt := st.chat_input("Ask anything about the Responible sessions..."):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
